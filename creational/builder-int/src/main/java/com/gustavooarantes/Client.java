c5d39bf82df670138beb5f7856c53d746197dbf1
gustavooarantes <profissional.gustavoa@gmail.com>
2025-09-16 16:23:36 -0300

    Use case of builder completed, now we move on to Simple Factory xD

--- creational/builder-int/src/main/java/com/gustavooarantes/Client.java
+++ creational/builder-int/src/main/java/com/gustavooarantes/Client.java
@@ -1,8 +1,31 @@
 package com.gustavooarantes;
 
+import com.gustavooarantes.UserDTO.UserDTOBuilder;
+
 public class Client {
 
 	public static void main(String[] args) {
+		User user = createUser();
 
+		UserDTO dto = directBuild(user);
+		System.out.println(dto);
+	}
+
+	private static UserDTO directBuild(User user) {
+		return new UserDTOBuilder()
+				.withName(user.getName())
+				.withAddress(user.getAddress())
+				.withAge(user.getAge())
+				.build();
+	}
+
+	public static User createUser() {
+		User user = new User();
+
+		user.setName("Gustavo");
+		user.setAddress("Goiânia");
+		user.setAge(22);
+
+		return user;
 	}
 }
---
+++ creational/builder-int/src/main/java/com/gustavooarantes/User.java
@@ -0,0 +1,34 @@
+package com.gustavooarantes;
+
+public class User {
+
+	private String name;
+
+	private String address;
+
+	private int age;
+
+	public String getName() {
+		return name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
